97d9aa57f49e81691777d8261e6ebeb6eacdd37a
ADITYA LELLA <128022129+adityasneo@users.noreply.github.com>
2023-07-13 23:41:48 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,1 +1,36 @@
+terraform {
+  required_version = ">= 1.2"
 
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.11, < 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = var.resource_group_name
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.azurerm_container_registry
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Premium"
+  admin_enabled       = false
+  georeplications {
+    location                = "Central India"
+    zone_redundancy_enabled = true
+    tags                    = {}
+  }
