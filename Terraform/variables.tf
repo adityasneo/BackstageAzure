5ee8617d63aecbe4e3185a1a84ee4ec9a6ee0526
ADITYA LELLA <128022129+adityasneo@users.noreply.github.com>
2023-07-13 23:44:22 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -15,3 +15,27 @@ variable "azurerm_container_registry" {
   default     = "backstageacr"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
+
+variable "cluster_name" {
+  type        = string
+  default     = "backstageaks"
+  description = "AKS name in Azure"
+}
+
+variable "kubernetes_version" {
+  type        = string
+  default     = "1.24.9"
+  description = "Kubernetes version"
+}
+
+variable "system_node_count" {
+  type        = number
+  default     = "2"
+  description = "Number of AKS worker nodes"
+}
+
+variable "node_resource_group" {
+  type        = string
+  default     = "backstagenoderg"
+  description = "RG name for cluster resources in Azure"
+}
