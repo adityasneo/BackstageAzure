381f503ff680f9f0b9c7087f734a59b4d4e4ca09
ADITYA LELLA <128022129+adityasneo@users.noreply.github.com>
2023-07-14 00:29:54 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,3 +1,15 @@
+variable "resource_group_location" {
+  type        = string
+  default     = "Central India"
+  description = "Location of the resource group."
+}
+
+variable "resource_group_name" {
+  type        = string
+  default     = "backstage"
+  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+}
+
 variable "azurerm_container_registry" {
   type        = string
   default     = "backstageacr"
